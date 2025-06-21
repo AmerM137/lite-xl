c78c06547a9a242fbc7dcabc4686b5a986a1d5ee
amr11 <mneimneh.amer@gmail.com>
2025-06-21 13:47:23 -0500

    skip subprojects

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 meson setup --wrap-mode=forcefallback --buildtype=release build
 meson compile -C build
-meson install -C build --skip-subprojects="freetype2,pcre2,sdl2" --destdir ../lite-xl/
+meson install -C build --skip-subprojects="freetype2,pcre2,sdl2,lua" --destdir ../lite-xl/
 mkdir ./lite-xl/user
